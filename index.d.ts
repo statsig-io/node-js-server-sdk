485418adeb2fc8b988985f0302c5ae360517d5bf
Jiakan Wang <jkw@statsig.io>
2021-04-02 13:06:33 -0700

    restrict event metadata and user custom object types

--- index.d.ts
+++ index.d.ts
@@ -12,7 +12,7 @@ declare module 'statsig-node-js-server-sdk' {
     country?: string;
     locale?: string;
     clientVersion?: string;
-    custom?: Record<string, any>;
+    custom?: Record<string, string | number | boolean | Array<string>>;
   };
 
   /**
@@ -54,14 +54,14 @@ declare module 'statsig-node-js-server-sdk' {
    * Log an event for data analysis and alerting or to measure the impact of an experiment
    * @param {StatsigUser} user - the user associated with this event
    * @param {string} eventName - the name of the event (name = Purchase)
-   * @param {string | number} value - the value associated with the event (value = 10)
-   * @param {object} metadata - other attributes associated with this event (metadata = {items: 2, currency: USD})
+   * @param {?string | number} value - the value associated with the event (value = 10)
+   * @param {?Record<string, string>} metadata - other attributes associated with this event (metadata = {item_name: 'banana', currency: 'USD'})
    */
   export function logEvent(
     user: StatsigUser | null,
     name: string,
     value?: string | number,
-    metadata?: object
+    metadata?: Record<string, string>
   ): void;
 
   /**
