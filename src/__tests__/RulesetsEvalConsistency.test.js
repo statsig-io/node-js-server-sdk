f5d5e70f8a280d99dd1351b5c06c0c63b2b8ac37
Jiakan Wang <jkw@statsig.io>
2021-07-08 11:27:14 -0700

    feedback

--- src/__tests__/RulesetsEvalConsistency.test.js
+++ src/__tests__/RulesetsEvalConsistency.test.js
@@ -30,62 +30,47 @@ if (secret) {
         body: JSON.stringify({}),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
-          'STATSIG-API-KEY':
-            'secret-9IWfdzNwExEYHEW4YfOQcFZ4xreZyFkbOXHaNbPsMwW',
+          'STATSIG-API-KEY': secret,
           'STATSIG-CLIENT-TIME': Date.now(),
         },
       };
-      await fetch('http://api.statsig.com/v1/rulesets_e2e_test', params)
-        .then((res) => {
-          return res.json();
-        })
+      const fetchProdPromise = fetch(
+        'http://api.statsig.com/v1/rulesets_e2e_test',
+        params,
+      )
+        .then((res) => res.json())
         .then((json) => {
           prodTestData = json.data;
         });
-      await fetch('http://latest.api.statsig.com/v1/rulesets_e2e_test', params)
-        .then((res) => {
-          return res.json();
-        })
+      const fetchStagingPromise = fetch(
+        'http://latest.api.statsig.com/v1/rulesets_e2e_test',
+        params,
+      )
+        .then((res) => res.json())
         .then((json) => {
           stagingTestData = json.data;
         });
+      await Promise.all([fetchProdPromise, fetchStagingPromise]);
     });
 
     test('server and SDK evaluates gates to the same results on production', async () => {
-      const totalChecks =
-        prodTestData.length *
-        (Object.keys(prodTestData[0].feature_gates).length +
-          Object.keys(prodTestData[0].dynamic_configs).length);
-      expect.assertions(totalChecks);
+      await validateServerSDKConsistency(prodTestData);
+    });
 
-      await statsig.initialize(secret);
-      const promises = prodTestData.map(async (data) => {
-        const user = data.user;
-        const gates = data.feature_gates;
-        const configs = data.dynamic_configs;
-        for (const name in gates) {
-          const sdkValue = await statsig.checkGate(user, name);
-          expect(sdkValue).toEqual(gates[name]);
-        }
-
-        for (const name in configs) {
-          const sdkValue = await statsig.getConfig(user, name);
-          expect(sdkValue.value).toMatchObject(configs[name].value);
-        }
-      });
-      await Promise.all(promises);
+    test('server and SDK evaluates gates to the same results on staging', async () => {
+      await validateServerSDKConsistency(stagingTestData);
     });
   });
 
-  test('server and SDK evaluates gates to the same results on staging', async () => {
+  async function validateServerSDKConsistency(testData) {
     const totalChecks =
-      stagingTestData.length *
-      (Object.keys(stagingTestData[0].feature_gates).length +
-        Object.keys(stagingTestData[0].dynamic_configs).length);
+      testData.length *
+      (Object.keys(testData[0].feature_gates).length +
+        Object.keys(testData[0].dynamic_configs).length);
     expect.assertions(totalChecks);
 
     await statsig.initialize(secret);
-    const promises = stagingTestData.map(async (data) => {
+    const promises = testData.map(async (data) => {
       const user = data.user;
       const gates = data.feature_gates;
       const configs = data.dynamic_configs;
@@ -100,12 +85,12 @@ if (secret) {
       }
     });
     await Promise.all(promises);
-  });
+  }
 } else {
   describe('', () => {
     test('Intended failing test. Proceed with pull request unless you are a Statsig employee.', () => {
       console.log(
-        'THIS TEST IS EXPECTED TO FAIL FOR NON-STATSIG EMPLOYEES! If this is the only test failing, please proceed to submit a pull request. If you are a Statsig employee, you need the ops repo.',
+        'THIS TEST IS EXPECTED TO FAIL FOR NON-STATSIG EMPLOYEES! If this is the only test failing, please proceed to submit a pull request. If you are a Statsig employee, chat with jkw.',
       );
       expect(true).toBe(false);
     });
