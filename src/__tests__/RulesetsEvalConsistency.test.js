5e6ba20823e2b9ebf97f60092534c5f221c949d6
Jiakan Wang <jkw@statsig.io>
2021-07-16 12:18:56 -0700

    refactor tests a bit

--- src/__tests__/RulesetsEvalConsistency.test.js
+++ src/__tests__/RulesetsEvalConsistency.test.js
@@ -22,68 +22,16 @@ if (secret) {
       jest.resetModules();
     });
 
-    test('server and SDK evaluates gates to the same results on production', async () => {
-      await validateServerSDKConsistency('https://api.statsig.com/v1');
-    });
-
-    test('server and SDK evaluates gates to the same results on US West', async () => {
-      await validateServerSDKConsistency('https://us-west-2.api.statsig.com/v1');
-    });
-
-    test('server and SDK evaluates gates to the same results on US East', async () => {
-      await validateServerSDKConsistency('https://us-east-2.api.statsig.com/v1');
-    });
-
-    test('server and SDK evaluates gates to the same results on AP South', async () => {
-      await validateServerSDKConsistency('https://ap-south-1.api.statsig.com/v1');
-    });
-
-    test('server and SDK evaluates gates to the same results on staging', async () => {
-      await validateServerSDKConsistency('https://latest.api.statsig.com/v1');
-    });
+    [
+      'https://api.statsig.com/v1',
+      'https://us-west-2.api.statsig.com/v1',
+      'https://us-east-2.api.statsig.com/v1',
+      'https://ap-south-1.api.statsig.com/v1',
+      'https://latest.api.statsig.com/v1'
+    ].map(url => test(`server and SDK evaluates gates to the same results on ${url}`, async () => {
+      await _validateServerSDKConsistency(url);
+    }));
   });
-
-  async function validateServerSDKConsistency(api) {
-    const response = await fetch(api + '/rulesets_e2e_test', {
-      method: 'POST',
-      body: JSON.stringify({}),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        'STATSIG-API-KEY': secret,
-        'STATSIG-CLIENT-TIME': Date.now(),
-      },
-    });
-    const testData = (await response.json()).data;
-
-    const totalChecks =
-      testData.length *
-      (Object.keys(testData[0].feature_gates).length +
-        Object.keys(testData[0].dynamic_configs).length);
-    expect.assertions(totalChecks);
-
-    const statsig = require('../index');
-      await statsig.initialize(secret, {api: api});
-    
-    const promises = testData.map(async (data) => {
-      const user = data.user;
-      const gates = data.feature_gates;
-      const configs = data.dynamic_configs;
-      for (const name in gates) {
-        const sdkValue = await statsig.checkGate(user, name);
-        if (sdkValue !== gates[name]) {
-          console.log(`Test failed for gate ${name}. Server got ${gates[name]}, SDK got ${sdkValue}`);
-        }
-        expect(sdkValue).toEqual(gates[name]);
-      }
-
-      for (const name in configs) {
-        const sdkValue = await statsig.getConfig(user, name);
-        expect(sdkValue.value).toMatchObject(configs[name].value);
-      }
-    });
-    await Promise.all(promises);
-    console.log(`Successfully completed ${totalChecks} checks for ${api}`);
-  }
 } else {
   describe('', () => {
     test('Intended failing test. Proceed with pull request unless you are a Statsig employee.', () => {
@@ -93,4 +41,46 @@ if (secret) {
       expect(true).toBe(false);
     });
   });
+}
+
+async function _validateServerSDKConsistency(api) {
+  const response = await fetch(api + '/rulesets_e2e_test', {
+    method: 'POST',
+    body: JSON.stringify({}),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      'STATSIG-API-KEY': secret,
+      'STATSIG-CLIENT-TIME': Date.now(),
+    },
+  });
+  const testData = (await response.json()).data;
+
+  const totalChecks =
+    testData.length *
+    (Object.keys(testData[0].feature_gates).length +
+      Object.keys(testData[0].dynamic_configs).length);
+  expect.assertions(totalChecks);
+
+  const statsig = require('../index');
+    await statsig.initialize(secret, {api: api});
+  
+  const promises = testData.map(async (data) => {
+    const user = data.user;
+    const gates = data.feature_gates;
+    const configs = data.dynamic_configs;
+    for (const name in gates) {
+      const sdkValue = await statsig.checkGate(user, name);
+      if (sdkValue !== gates[name]) {
+        console.log(`Test failed for gate ${name}. Server got ${gates[name]}, SDK got ${sdkValue}`);
+      }
+      expect(sdkValue).toEqual(gates[name]);
+    }
+
+    for (const name in configs) {
+      const sdkValue = await statsig.getConfig(user, name);
+      expect(sdkValue.value).toMatchObject(configs[name].value);
+    }
+  });
+  await Promise.all(promises);
+  console.log(`Successfully completed ${totalChecks} checks for ${api}`);
 }
