dae4fc4696a6277f855c0a15912ecc18778ebe9e
kenny-statsig <111380336+kenny-statsig@users.noreply.github.com>
2023-12-27 16:36:24 -0800

    Enforce pretty lint rules (#397)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,45 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   plugins: ['@typescript-eslint', 'simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['off', { varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-for-in-array': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': ['warn', { ignoreVoid: true }],
+    '@typescript-eslint/restrict-plus-operands': 'warn',
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  ignorePatterns: ['**/node_modules/*', '**/dist/*'],
+  ignorePatterns: [
+    '**/node_modules/*',
+    '**/dist/*',
+    '**/*.test.ts',
+    '**/__tests__/*.ts',
+  ],
   overrides: [
-    // Tests
-    {
-      files: ['**/*.test.ts', '**/__tests__/*.ts'],
-      rules: {
-        '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-      },
-    },
     // Exceptions
     {
       files: ['**/safeFetch.ts'],
